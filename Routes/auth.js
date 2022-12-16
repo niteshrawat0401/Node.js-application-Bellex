71cffc10faaec685ea3c0c1045aa199eb627d9e2
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-12-16 23:55:36 +0530

    validation error

--- Routes/auth.js
+++ Routes/auth.js
@@ -3,6 +3,8 @@ const User= require("../model/authuser")
 
 const authRouter= Router();
 
+
+// Sign up
 authRouter.post("/signup", async(req,res)=>{
     const newuser = await new User(req.body);
     newuser.save((err, success) => {
@@ -16,6 +18,7 @@ authRouter.post("/signup", async(req,res)=>{
       });
 })
 
+// username alrady exist
 authRouter.post("/userexists", async(req,res)=>{
     const { username } = req.body;
     const user = await User.findOne( {username} )
@@ -26,5 +29,19 @@ authRouter.post("/userexists", async(req,res)=>{
             .send({success: false ,message: `username ${user} already present`});
 })
 
+// Validation errors
+authRouter.post("/usererr", async(req,res)=>{
+    const { username } = req.body;
+    const user = await User.findOne( {username} )
+    console.log(user);
+            if(!user)
+          return res
+            .status(400)
+            .send({success: false ,message: `username ${user} already present`});
+})
+
+
+// Log In
+// authRouter.post
 
 module.exports= authRouter
